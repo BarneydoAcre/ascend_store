7b08e75003ddc565c7c301acf01e39a7a0b404c9
unknown <gabrielfauth@outlook.com>
2021-12-14 13:48:45 -0400

    atualização 14/12/2021

--- app/static/js/js.js
+++ app/static/js/js.js
@@ -1,18 +1,27 @@
-// try{
-//     const sair = document.querySelector('.btn-sair')
-//     sair.addEventListener('click', function(event){
-//         if(confirm('Deseja mesmo sair?')){
-//             return true;
-//         }else{
-//             event.preventDefault();
-//         }
-//     })
-// }catch (e){
-//     console.log(e)
-// }
+// (function(win,doc){
+//     'use strict';
 
-function mudar(i){
-    console.log(
-        i
-    )
-}
+//     if(document.querySelector('.btn-add-shop-car')){
+//         let form = document.querySelector('.btn-add-shop-car')
+//         function sendForm(event){
+//             event.preventDefault();
+//             let data = new FormData(form)
+//             let ajax = new XMLHttpRequest();
+//             let token = doc.querySelectorAll('input')[0].value;
+//             ajax.open('POST', form.action);
+//             ajax.setRequestHeader('X-CSRF-TOKEN',token);
+//             ajax.onreadystatechange = function()
+//             {
+//                 if(ajax.status === 200 && ajax.readyState === 4){
+//                     let result = doc.querySelector('#result');
+//                     result.innerHTML = 'Operação realizada com sucesso!';
+//                     result.classList.add('alert');
+//                     result.classList.add('alert-success');
+//                 }
+//             }
+//             ajax.send(data);
+//             form.reset();
+//         }
+//         form.addEventListener('submit',sendForm,false);
+//     }
+// })(window,document);
