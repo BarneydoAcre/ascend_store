28e771215c7b7cd2e2c754c75a45b0d35bbadefc
unknown <gabrielfauth@outlook.com>
2022-01-28 01:24:36 -0400

    t

--- app/views.py
+++ app/views.py
@@ -135,7 +135,7 @@ def shop_car_delete(request):
 def notifications(request):
     response_data = {}
     if request.method == "POST":
-        models.MercadoPagoNotification(topic=request.GET['topic'],id_topic=request.GET['id'],json=None).save()
+        models.MercadoPagoNotification(json=str(request.POST)).save()
         return HttpResponse(json.dumps(response_data), content_type="application/json", status=201)
 
     elif request.method == "GET":
